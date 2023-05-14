67ed43f498b73275500ea1261b8537b7da294742
Gustavo Loures <loures_pc@Gustavos-MacBook-Pro.local>
2023-05-13 21:34:34 -0300

    Initializing project

---
+++ Project2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Gustavo Loures on 5/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var Opcao1: UIButton!
+    @IBOutlet var Opcao2: UIButton!
+    @IBOutlet var opcao3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
